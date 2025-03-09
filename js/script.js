8fedbb2a6f37b5c806913d01253468809ece9fc1
PC <sobolinskii@mail.ru>
2025-03-09 22:49:11 +0800

    update new pages

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 $(document).ready(function () {
-	const swiper = new Swiper('.main-slider', {
-		slidesPerView: 1,
-		spaceBetween: 20,
-		loop: true,
-		pagination: {
-			el: '.swiper-pagination',
-			clickable: true,
-		},
-		speed: 1200,
-		navigation: {
-			nextEl: '.main-swiper-navigation__button--next',
-			prevEl: '.main-swiper-navigation__button--prev',
-		},
-	})
+	if ($('.main-slider').length !== 0) {
+		const swiper = new Swiper('.main-slider', {
+			slidesPerView: 1,
+			spaceBetween: 20,
+			loop: true,
+			pagination: {
+				el: '.swiper-pagination',
+				clickable: true,
+			},
+			speed: 1200,
+			navigation: {
+				nextEl: '.main-swiper-navigation__button--next',
+				prevEl: '.main-swiper-navigation__button--prev',
+			},
+		})
+	}
 
 	// Открытие модалки
 	$('[data-modal-target]').on('click', function () {
@@ -28,6 +30,27 @@ $(document).ready(function () {
 	$('.modal-wrapper').on('click', function (e) {
 		if ($(e.target).hasClass('modal-wrapper')) {
 			$(this).addClass('hide')
+		}
+	})
+
+	// custom select
+	$('.custom-select .selected-option').click(function () {
+		$(this).parent().toggleClass('open')
+	})
+
+	$('.custom-select .options li').click(function () {
+		var value = $(this).data('value')
+		var text = $(this).text()
+		$(this)
+			.closest('.custom-select')
+			.find('.selected-option span:first')
+			.text(text)
+		$(this).closest('.custom-select').removeClass('open')
+	})
+
+	$(document).click(function (e) {
+		if (!$(e.target).closest('.custom-select').length) {
+			$('.custom-select').removeClass('open')
 		}
 	})
 })
