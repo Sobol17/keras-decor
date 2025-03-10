add5796789d6a325449d84b23eaa2d44d1814c4c
PC <sobolinskii@mail.ru>
2025-03-10 21:41:40 +0800

    complete design page

--- js/script.js
+++ js/script.js
@@ -58,20 +58,93 @@ $(document).ready(function () {
 
 	const images = $('.work-content__img img')
 	const steps = $('.work-step')
+	const prevBtn = $('.work-nav__prev')
+	const nextBtn = $('.work-nav__next')
+
+	let circleIndex = 0
+
+	function updateDisplay() {
+		images.addClass('hide')
+		images.eq(circleIndex).removeClass('hide')
+		steps.removeClass('active')
+		steps.eq(circleIndex).addClass('active')
+
+		if (window.innerWidth < 650) {
+			$('.steps').css(
+				'transform',
+				`translateX(-50%) rotate(-${90 * circleIndex}deg)`
+			)
+			if (circleIndex % 2 === 0) {
+				steps.css(
+					'transform',
+					`translateX(-50%) rotate(${90 * circleIndex}deg)`
+				)
+			} else {
+				steps.css(
+					'transform',
+					`translateY(-50%) rotate(${90 * circleIndex}deg)`
+				)
+			}
+		}
+	}
 
 	steps.on('click', function () {
-		const index = $(this).index()
-		images.addClass('hide')
-		images.eq(index).removeClass('hide')
-		steps.removeClass('active')
-		steps.eq(index).addClass('active')
-		if (window.innerWidth < 650) {
-			$('.steps').css('transform', `translateX(-50%) rotate(-${90 * index}deg)`)
-			if (index % 2 === 0) {
-				steps.css('transform', `translateX(-50%) rotate(-${90 * index}deg)`)
-			} else {
-				steps.css('transform', `translateY(-50%) rotate(-${90 * index}deg)`)
-			}
+		circleIndex = $(this).index()
+		updateDisplay()
+	})
+
+	prevBtn.on('click', function () {
+		circleIndex = (circleIndex - 1 + images.length) % images.length
+		updateDisplay()
+	})
+
+	nextBtn.on('click', function () {
+		circleIndex = (circleIndex + 1) % images.length
+		updateDisplay()
+	})
+
+	// tel input mask
+	function applyPhoneMask(input) {
+		let value = input.value.replace(/\D/g, '')
+		let formattedValue = '+7'
+
+		if (value.length > 1) {
+			value = value.substring(1)
+		}
+
+		if (value.length > 0) {
+			formattedValue += '(' + value.substring(0, 3)
+		}
+		if (value.length > 3) {
+			formattedValue += ')-' + value.substring(3, 6)
+		}
+		if (value.length > 6) {
+			formattedValue += '-' + value.substring(6, 8)
+		}
+		if (value.length > 8) {
+			formattedValue += '-' + value.substring(8, 10)
+		}
+
+		input.value = formattedValue
+	}
+
+	$('input[type="tel"]').on('input', function () {
+		applyPhoneMask(this)
+	})
+
+	$('input[type="tel"]').on('focus', function () {
+		if (!this.value) {
+			this.value = '+7'
+		}
+	})
+
+	$('input[type="tel"]').on('keydown', function (e) {
+		if (
+			this.value.length >= 17 &&
+			e.key !== 'Backspace' &&
+			e.key !== 'Delete'
+		) {
+			e.preventDefault()
 		}
 	})
 })
