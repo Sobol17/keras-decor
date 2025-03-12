b185851376ee2d376ba47fc238ca7152196c47bd
sobol17 <sobolinskii@mail.ru>
2025-03-12 11:36:56 +0800

    add fancy boxes and update scripts

--- js/script.js
+++ js/script.js
@@ -203,21 +203,23 @@ $(document).ready(function () {
 	let initialMin = $('#range-slider').data('initial-min')
 	let initialMax = $('#range-slider').data('initial-max')
 
-	$('#range-slider').slider({
-		range: true,
-		min: initialMin,
-		max: initialMax,
-		values: ['1000', '9000'],
-		slide: function (event, ui) {
-			$('#min').val(ui.values[0])
-			$('#max').val(ui.values[1])
-		},
-		classes: {
-			'ui-slider': 'range-slider',
-			'ui-slider-handle': 'range-slider__handle',
-			'ui-slider-range': 'range-slider__range',
-		},
-	})
+	if ($('#range-slider')) {
+		$('#range-slider').slider({
+			range: true,
+			min: initialMin,
+			max: initialMax,
+			values: ['1000', '9000'],
+			slide: function (event, ui) {
+				$('#min').val(ui.values[0])
+				$('#max').val(ui.values[1])
+			},
+			classes: {
+				'ui-slider': 'range-slider',
+				'ui-slider-handle': 'range-slider__handle',
+				'ui-slider-range': 'range-slider__range',
+			},
+		})
+	}
 
 	// dropdown
 	$('.sidebar-filter__title').on('click', function () {
@@ -227,4 +229,143 @@ $(document).ready(function () {
 	$('.catalog-content__filter--mobile').on('click', function () {
 		$('.sidebar').addClass('active')
 	})
+
+	$('.catalog-content__grid-2').on('click', function () {
+		$('.catalog-list').addClass('col-2')
+	})
+
+	$('.catalog-content__grid-3').on('click', function () {
+		$('.catalog-list').removeClass('col-2')
+	})
+
+	// tabs
+	$('.product-tabs__item').on('click', function () {
+		var tabId = $(this).data('tab')
+
+		$('.product-tabs__item').removeClass('active')
+		$('.product-tabs__content').removeClass('active')
+
+		$(this).addClass('active')
+		$('#' + tabId).addClass('active')
+	})
+
+	// formatter
+	const formatter = new Intl.NumberFormat('ru-RU', {
+		style: 'decimal',
+		minimumFractionDigits: 2,
+		maximumFractionDigits: 2,
+	})
+
+	// cart item quantity
+	const cartItemPlus = $('.cart-item__count .cart-plus')
+	const cartItemMinus = $('.cart-item__count .cart-minus')
+
+	cartItemPlus.on('click', function () {
+		const price = $(this).parents('.cart-item').data('cart-price')
+		const cartItemCounter = $(this)
+			.parents('.cart-item')
+			.find('.cart-item__count__input')
+		const cartItemSumContainer = $(this)
+			.parents('.cart-item')
+			.find('.cart-item__summ-num')
+		let countValue = parseInt(cartItemCounter.val(), 10)
+
+		countValue += 1
+		cartItemCounter.val(countValue)
+		cartItemSumContainer.text(formatter.format(price * countValue) + '₽')
+	})
+
+	cartItemMinus.on('click', function () {
+		const price = $(this).parents('.cart-item').data('cart-price')
+		const cartItemCounter = $(this)
+			.parents('.cart-item')
+			.find('.cart-item__count__input')
+		const cartItemSumContainer = $(this)
+			.parents('.cart-item')
+			.find('.cart-item__summ-num')
+		let countValue = parseInt(cartItemCounter.val(), 10)
+
+		if (countValue > 1) {
+			countValue -= 1
+			cartItemCounter.val(countValue)
+			cartItemSumContainer.text(formatter.format(price * countValue) + '₽')
+		}
+	})
+
+	// calculate product sum
+	const price = $('[data-price]').data('price')
+	const packingSize = $('[data-size]').data('size')
+
+	// ui refs
+	const btnMinus = $('.product-controls__count .cart-minus')
+	const btnPLus = $('.product-controls__count .cart-plus')
+	const countInput = $('.product-controls__count input')
+	const areaInput = $('#area-input')
+	const actualAreaRef = $('#actual-area')
+	const totalSumRef = $('#total-sum')
+
+	function updateUI() {
+		const areaValue = parseInt(areaInput.val(), 10) || 0
+		const countValue = parseInt(countInput.val(), 10) || 0
+
+		const actualArea = (areaValue * packingSize).toFixed(2) + ' м²'
+		const totalSum = formatter.format(price * areaValue)
+
+		actualAreaRef.text(actualArea)
+		totalSumRef.text(totalSum)
+
+		countInput.val(Math.ceil(areaValue / packingSize))
+	}
+
+	areaInput.on('input', function () {
+		let value = parseInt($(this).val(), 10) || 0
+
+		if (value > 9999) {
+			value = 9999
+			$(this).val(value)
+		}
+
+		updateUI()
+	})
+
+	countInput.on('input', function () {
+		let countValue = parseInt($(this).val(), 10) || 0
+
+		if (countValue < 1) {
+			countValue = 1
+			$(this).val(countValue)
+		}
+
+		const areaValue = countValue * packingSize
+		areaInput.val(areaValue)
+
+		updateUI()
+	})
+
+	btnPLus.on('click', function () {
+		let countValue = parseInt(countInput.val(), 10) || 0
+		countValue += 1
+		countInput.val(countValue)
+
+		areaInput.val(countValue * packingSize)
+		updateUI()
+	})
+
+	btnMinus.on('click', function () {
+		let countValue = parseInt(countInput.val(), 10) || 0
+
+		if (countValue > 1) {
+			countValue -= 1
+			countInput.val(countValue)
+
+			areaInput.val(countValue * packingSize)
+			updateUI()
+		}
+	})
+
+	updateUI()
+
+	if ($('[data-fancybox]').length !== 0) {
+		Fancybox.bind('[data-fancybox]')
+	}
 })
